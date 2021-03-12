8351c22313e1bf7bc3dafb7063af7944fc5bf0ec
Jane Abyss <nepoznat77@gmail.com>
2021-03-12 17:29:21 +0300

    feat: typical arrays problems solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (!array || array.length === 0) return 0;
+    array.sort((a, b) => { return a - b });
+    return array[0];
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!array || array.length === 0) return 0;
+    array.sort((a, b) => { return a - b });
+    return array[array.length - 1];
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!array || array.length === 0) return 0;
+    return array.reduce((counter, item) => (counter + item), 0) / array.length;
 }
